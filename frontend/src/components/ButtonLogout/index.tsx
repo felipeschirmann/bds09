7308af21964ce6123b3fb5a4383ea98305ca1aca
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-07-08 16:35:33 -0300

    Create and desing button login

---
+++ frontend/src/components/ButtonLogout/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+const ButtonLogout = () => {
+  return (
+    <>
+      <button className="btn btn-primary btn-logout">
+        <h6>SAIR</h6>
+      </button>
+    </>
+  );
+};
+
+export default ButtonLogout;
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,20 +1,27 @@
-import './styles.css';
+import ButtonLogout from "components/ButtonLogout";
+import "./styles.css";
+
 const Navbar = () => {
   return (
     <>
       <nav className="bg-primary navbar-expand-md bg-primary main-nav">
         <div className="container-fluid">
-          <div className="nav-logo-text">
-            <a href="/">
-              <h4>MovieFlix</h4>
-            </a>
-          </div>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav offset-md-3 main-menu">
-              <li>
-                <a href="/" className='active' ></a>
-              </li>
-            </ul>
+          <div className="nav-navigation-itens">
+            <div className="nav-container-navigation">
+              <div className="nav-logo-text">
+                <a href="/">
+                  <h4>MovieFlix</h4>
+                </a>
+              </div>
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav offset-md-3 main-menu">
+                  <li>
+                    <a href="/" className="active"></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <ButtonLogout />
           </div>
         </div>
       </nav>
