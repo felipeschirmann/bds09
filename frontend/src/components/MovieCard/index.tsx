7651a030319ac1e33e8c7b12048fd341b03be943
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-11-02 17:30:41 -0300

    movie details set dinamic content

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,11 +1,21 @@
-import { Movie } from "types/movie";
 import "./styles.css";
 
 type Props = {
-  movie: Movie;
+  movieid: number;
 };
 
-const MovieCard = ({ movie }: Props) => {
+const MovieCard = ({ movieid }: Props) => {
+  const movie = {
+    id: 1,
+    title: "Bob Esponja",
+    subTitle: "O Incrível Resgate",
+    year: 2020,
+    imgUrl:
+      "https://image.tmdb.org/t/p/w533_and_h300_bestv2/wu1uilmhM4TdluKi2ytfz8gidHf.jpg",
+    synopsis:
+      'Onde está Gary? Segundo Bob Esponja, Gary foi "caracolstrado" pelo temível Rei Poseidon e levado para a cidade perdida de Atlantic City. Junto a Patrick Estrela, ele sai em uma missão de resgate ao querido amigo, e nesta jornada os dois vão conhecer novos personagens e viver inimagináveis aventuras.',
+  };
+
   return (
     <>
       <div className="card-movie">
--- frontend/src/pages/Private/MovieDetails/index.tsx
+++ frontend/src/pages/Private/MovieDetails/index.tsx
@@ -2,6 +2,7 @@ import Navbar from "components/Navbar";
 import ReviewListing from "components/ReviewListing";
 import ReviewForm from "components/ReviewForm";
 import { Review } from "types/review";
+import { Movie } from "types/movie";
 import { BASE_URL, hasAnyRoles, requestBackend } from "util/requests";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -16,7 +17,9 @@ const MovieDetails = () => {
   const { movieId } = useParams<UrlParams>();
 
   const [reviews, setReviews] = useState<Review[]>([]);
-
+  
+  const [movie, setMovie] = useState<Movie>();
+  
   useEffect(() => {
     const config: AxiosRequestConfig = {
       url: `${BASE_URL}/movies/${movieId}/reviews`,
@@ -34,25 +37,27 @@ const MovieDetails = () => {
     setReviews(clone);
   };
 
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      url: `${BASE_URL}/movies/${movieId}`,
+      withCredentials: true,
+    };
+    requestBackend(config).then((response) => {
+      setMovie(response.data);
+      console.log(response.data);
+    });
+  }, [movieId]);
+
   return (
     <>
       <Navbar />
       <div className="container-movie-details">
         <div className="card-movie-details">
-          <img
-            src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/wu1uilmhM4TdluKi2ytfz8gidHf.jpg"
-            alt=""
-          />
-          <h6>Bob Esponja</h6>
-          <p className="card-movie-year">2020</p>
-          <div className="subtitle-details">O Incrível Resgate</div>
-          <div className="description">
-            Onde está Gary? Segundo Bob Esponja, Gary foi "caracolstrado"
-            pelo temível Rei Poseidon e levado para a cidade perdida de Atlantic
-            City. Junto a Patrick Estrela, ele sai em uma missão de resgate ao
-            querido amigo, e nesta jornada os dois vão conhecer novos
-            personagens e viver inimagináveis aventuras.
-          </div>
+          <img src={movie?.imgUrl} alt={movie?.title} />
+          <h6>{movie?.title}</h6>
+          <p className="card-movie-year">{movie?.year}</p>
+          <div className="subtitle">{movie?.subTitle}</div>
         </div>
         {hasAnyRoles(["ROLE_MEMBER"]) && movieId ? (
           <ReviewForm movieId={movieId} onInsertReview={handleInsertReview} />
