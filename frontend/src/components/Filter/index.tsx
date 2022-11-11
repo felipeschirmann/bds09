8b17e3956417ee58e261af83d6d5c39c24703296
Felipe Schirmann <contato@felipeschirmann.dev.br>
2022-11-11 15:05:17 -0300

    populate the select of genres

--- frontend/src/components/Filter/index.tsx
+++ frontend/src/components/Filter/index.tsx
@@ -1,12 +1,55 @@
+import { AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import Select from "react-select";
+import { Genre } from "types/genre";
+import { BASE_URL, requestBackend } from "util/requests";
+
 import "./styles.css";
 
 const Filter = () => {
+  const [selectGenres, setSelectGenres] = useState<Genre[]>([]);
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      url: `${BASE_URL}/genres`,
+      withCredentials: true,
+    };
+    requestBackend(config).then((response) => {
+      setSelectGenres(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
+  const { register, handleSubmit, control, getValues } = useForm<Genre>();
+
+  const onSubmit = (formData: Genre) => {
+    console.log(formData);
+  };
+
+  const handleSelectChange = (value: Genre) => {
+    console.log(value);
+  };
+
   return (
     <div className="movie-filter-container">
-      <form action="">
-        <select className="movie-filter" name="" id="">
-          <option value="aventura">Aventura</option>
-        </select>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Controller
+            name="name"
+            control={control}
+            render={() => (
+              <Select
+                options={selectGenres}
+                isClearable
+                classNamePrefix="movie-filter"
+                getOptionLabel={(genre: Genre) => genre.name}
+                getOptionValue={(genre: Genre) => String(genre.id)}
+                onChange={(value) => handleSelectChange(value as Genre)}
+              />
+            )}
+          />
+        </div>
       </form>
     </div>
   );
